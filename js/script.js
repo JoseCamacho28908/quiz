9d0b31a02aee02bbda8e1a6b907521a485901e83
Jose Camacho <josecamacho@students.ptechnyc.org>
2019-01-08 22:50:50 +0000

    Added a total score to the quiz

--- js/script.js
+++ js/script.js
@@ -1,14 +1,70 @@
 /* global $ */
+var name = $("#name").val();
+var totalScore = 0;
+var Pokemon = "TBD";
+var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
+var q1Result = $("#question1").val();
+var q2Result = $("#question2").val();
+var q3Result = $("#question3").val();
 
 $(document).ready(function() {
+
     $("button").click(function() {
-        var name = $("#name").val();
-        var techPlacement = "TBD";
-        var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
-        var q1Result = $("#question1").val();
-        var q2Result = $("#question2").val();
-        var q3Result = $("#question3").val();
-        var totalScore;
+        function question1(season) {
+            if (q1Result === "Fall") {
+                return totalScore + 1;
+            }
+            else if (q1Result === "Spring") {
+                return totalScore + 3;
+            }
+            else if (q1Result === "Winter") {
+                return totalScore + 5;
+            }
+            else if (q1Result === "Summer") {
+                return totalScore + 7;
+            }
+            else {
+                return totalScore + 0;
+            }
+        }
+
+        function question2() {
+            if (q2Result === "Blue") {
+                return totalScore + 5;
+            }
+            else if (q2Result === "Pink") {
+                return totalScore + 1;
+            }
+            else if (q2Result === "Red") {
+                return totalScore + 3;
+
+            }
+            else if (q2Result === "Orange") {
+                return totalScore + 7;
+
+            }
+
+            function question2() {
+                if (q3Result === "Math") {
+                    return totalScore + 5;
+                }
+                else if (q3Result === "ELA") {
+                    totalScore + 1;
+                }
+                else if (q3Result === "Science") {
+                    totalScore + 3;
+                }
+                else if (q3Result === "Social Studies") {
+                    totalScore + 7;
+
+                }
+                else {
+                    totalScore + 0;
+                }
+
+
+            }
+        }
     });
 
 });
