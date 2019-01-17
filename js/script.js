90768dba3da5ed5be6f08b8df39733a0f1023ef6
Jose Camacho <josecamacho@students.ptechnyc.org>
2019-01-17 03:17:21 +0000

    Finished website

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 /* global $ */
-var name = $("#name").val();
 var Pokemon = "TBD";
-var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
+var img="https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
 
 
 
 
+    
 $("button").click(function() {
+    
     var totalScore = 0;
+    var name = $("#name").val();
+   
+ 
+   
    
     var q1Result = $("#question1").val();
     totalScore = question1(q1Result, totalScore);
@@ -20,9 +25,15 @@ $("button").click(function() {
     var q3Result = $("#question3").val();
     totalScore = question3(q3Result, totalScore);
     scores(totalScore);
+    
+    
+   displayResult(Pokemon, name, img);
+    
 });
 
 
+
+    function placement(totalScore, name){}
 
 function question1(season, totalScore) {
     if (season === "Fall") {
@@ -85,19 +96,46 @@ function question3(subject, totalScore) {
 }
 
 function scores(totalScore) {
-    if (totalScore > 0 && totalScore <= 6) {
-        return $(".answer").html("You are a weedle");
+    
+    
+    if (totalScore >= 0 && totalScore <= 6) {
+        var name = $("#name").val();
+         Pokemon = "weedle";
+        displayResult(Pokemon,name,img);
+        img="https://media.giphy.com/media/anLxZPExDZZ4I/giphy.gif";
+        
     }
-    else if (totalScore > 7 && totalScore <= 13) {
-        return $(".answer").html("You are a pichu");
+    else if (totalScore >= 7 && totalScore <= 13) {
+      Pokemon = "pichu";
+        displayResult(Pokemon,name,img);
+        img="https://media.giphy.com/media/xX9tcQCgPRmDK/giphy.gif";
+        
     }
-    else if (totalScore > 14 && totalScore <= 18) {
-       return $(".answer").html("You are a Gyrados");
+    else if (totalScore >= 14 && totalScore <= 18) {
+     
+        Pokemon = "Gyrados";
+        displayResult(Pokemon,name,img);
+        img="https://media1.tenor.com/images/8a065c4fadd0f39457c3803c6beecdca/tenor.gif?itemid=7279879";
+        
     }
-    else if (totalScore > 19 && totalScore <= 21) {
-       return $(".answer").html("You are Greninja");
+     
+    else if (totalScore >= 19 && totalScore <= 21) {
+        
+        Pokemon = "Greninja";
+        displayResult(Pokemon,name,img);
+        img="https://media.giphy.com/media/12BtgqzcTMNrmE/giphy.gif";
+        
     }
     else{
          return $(".answer").html("You are pikachu");
     }
 }
+
+function displayResult(Pokemon,name, img){
+        return $(".result").html("<h2>Congratulations, " + name + " you're  " + Pokemon + "</h2>"+"<img src=" +img+">");
+
+    }
+
+    
+
+    
